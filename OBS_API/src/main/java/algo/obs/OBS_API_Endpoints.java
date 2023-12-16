4e82d2bbf13e15ed8410a7e5089ebbc0ae397201
Anthony Paschal <anthonydanielpaschal@gmail.com>
2023-12-16 16:08:45 -0500

    OBS API Endpoints scaffolded and ready for adding actual endpoints.

--- OBS_API/src/main/java/algo/obs/OBS_API_Endpoints.java
+++ OBS_API/src/main/java/algo/obs/OBS_API_Endpoints.java
@@ -1,4 +1,109 @@
 package algo.obs;
 
+import io.obswebsocket.community.client.OBSRemoteController;
+import spark.Spark;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 public class OBS_API_Endpoints {
+
+    private static final Map<String, Function<Map<String, String>, String>> endpoints = new HashMap<>();
+
+    OBSRemoteController controller;
+
+    public OBS_API_Endpoints(int port) {
+        setupObsController();
+        setupEndpointService(port);
+        defineEndpoints();
+    }
+
+    private void setupObsController() {
+
+        try {
+
+            controller = OBSRemoteController.builder()
+                .host("localhost")                  // Default host
+                .port(4455)                         // Default port
+                .password("OBS_PASSWORD")   // Provide your password here
+                .connectionTimeout(3)               // Seconds the client will wait for OBS to respond
+                .build();
+
+            controller.connect();
+
+        } catch (Exception ex) {
+            throw new RuntimeException("Failed to connect to OBS", ex);
+        }
+
+    }
+
+    private void defineEndpoints() {
+
+    /*
+        FORMAL OBS CONTROLLER ENDPOINTS:
+
+            Get Stream State (1/0)
+            Start Stream (return success/failure)
+            Stop Stream (return success/failure)
+            Get Scenes (json list)
+            Get Current Scene (return string)
+            Set Scene (return success/failure)
+     */
+
+        addEndpoint("start_stream", (params) -> {
+            System.out.println("start_stream");
+            // Add your custom logic here
+            return "json_string";
+        });
+
+    }
+
+    private void setupEndpointService(int port) {
+
+        // Create a Spark instance
+        Spark.port(port); // You can change the port as needed
+
+        // Define a dynamic endpoint that executes the associated runnable
+        Spark.get("/:endpointName", (request, response) -> {
+
+            // extract the name of the endpoint hit by this request i.e. url.com/endpointName
+            String endpointName = request.params(":endpointName");
+
+            // sanity check: does this endpoint exist in our map of endpoints?
+            if (endpoints.containsKey(endpointName)) {
+
+                // fetch the appropriate endpointFunction from our map of endpoints, if we have one
+                Function<Map<String, String>, String> endpointFunction = endpoints.get(endpointName);
+
+                // log the call & params
+                System.out.println("Endpoint Called with Params: ");
+                request.params().forEach((k, v) -> System.out.println("\t > " + k + " \t=\t " + v));
+
+                // execute the endpointFunction with the params from the request, retain the response
+                String responseStr = endpointFunction.apply(request.params());
+
+                // log the call & response
+                System.out.println("Endpoint executed: " + endpointName + "\n Our Response... \n " + responseStr);
+
+                // return the response, this will display in the browser
+                return responseStr;
+
+            } else {
+
+                response.status(418);
+
+                return "I am a tea pot.";
+
+            }
+        });
+
+
+    }
+
+    private static void addEndpoint(String endpointName, Function<Map<String, String>, String> endpointFunction) {
+        System.out.println("Adding Endpoint Function: " + endpointName);
+        endpoints.put(endpointName, endpointFunction);
+    }
+
 }
