451a4c367d3696d52a9f2468717497a0cd094a2c
Anthony Paschal <anthonydanielpaschal@gmail.com>
2023-12-19 15:13:05 -0500

    Added top-level exception handling.

--- TwitchWebsocketGateway/src/main/java/algo/twitch/websocket/Start_Here.java
+++ TwitchWebsocketGateway/src/main/java/algo/twitch/websocket/Start_Here.java
@@ -4,9 +4,15 @@ import java.net.InetSocketAddress;
 
 public class Start_Here {
     public static void main(String[] args) {
-        System.out.println("Starting...");
-        new TwitchOAuthEndpoint(80);
-        new WebsocketGateway(new InetSocketAddress("0.0.0.0", 8080)).start();
-        System.out.println("Goliath Online.");
+        try {
+
+            System.out.println("Starting...");
+            new TwitchOAuthEndpoint(80);
+            System.out.println("Endpoint Online.");
+            new WebsocketGateway(new InetSocketAddress("0.0.0.0", 8080)).start();
+            System.out.println("Gateway Online.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
