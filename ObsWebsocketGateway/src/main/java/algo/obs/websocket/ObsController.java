1a7826d2f666e179bf581f97d6ef73aa247a3d49
Anthony Paschal <anthonydanielpaschal@gmail.com>
2023-12-20 15:59:11 -0500

    Ready to Rock. Required a major refactor to the embed javascript.

---
+++ ObsWebsocketGateway/src/main/java/algo/obs/websocket/ObsController.java
@@ -0,0 +1,134 @@
+package algo.obs.websocket;
+
+import org.java_websocket.WebSocket;
+
+import java.util.function.Consumer;
+
+/**
+ * Represents a single OBS Embed session.
+ * Be gentle with it. This is somebody's live stream.
+ */
+public class ObsController {
+
+    private boolean isStreaming;
+    private String currentScene;
+
+    public final String hashId;
+
+    public final String[] scenes;
+    private final WebSocket webSocket;
+    public final String hostAddress;
+
+    public Consumer<Boolean> stateChangeListener;
+    public Consumer<String> sceneChangeListener;
+
+    public ObsController(String hashId, WebSocket webSocket, String currentScene, String[] scenes, boolean isStreaming) {
+        this.hashId = hashId;
+        this.webSocket = webSocket;
+        this.currentScene = currentScene;
+        this.scenes = scenes;
+        this.isStreaming = isStreaming;
+        this.hostAddress = webSocket.getRemoteSocketAddress().getAddress().getHostAddress();
+        log("Ready.");
+    }
+
+    /**
+     * Subscribes to the "Streaming State Change" event.
+     *
+     * @param stateChangeListener a lambda that accepts a boolean value, which will be called when the streaming state changes
+     */
+    public void subscribeToStreamingStateChanges(Consumer<Boolean> stateChangeListener) {
+        this.stateChangeListener = stateChangeListener;
+    }
+
+    /**
+     * Subscribes to the "Scene Change" event.
+     *
+     * @param sceneChangeListener a lambda that accepts a string value, which will be called when the scene changes
+     */
+    public void subscribeToSceneChanges(Consumer<String> sceneChangeListener) {
+        this.sceneChangeListener = sceneChangeListener;
+    }
+
+    /**
+     * Commands the OBS Embed to change the scene.
+     *
+     * @param sceneName the name of the scene to change to. Must be one of the scenes provided in the "scenes" array in the constructor.
+     */
+    public void changeScene(String sceneName) {
+        log("Changing Scene... | " + currentScene + " -> " + sceneName);
+        webSocket.send("{\"type\":\"set_scene\",\"sceneName\":\"" + sceneName + "\"}");
+    }
+
+    /**
+     * Commands the OBS Embed to start streaming.
+     */
+    public void startStreaming() {
+        log("Starting stream...");
+        webSocket.send("{\"type\":\"start_stream\"}");
+    }
+
+    /**
+     * Commands the OBS Embed to stop streaming.
+     */
+    public void stopStreaming() {
+        log("Stopping stream...");
+        webSocket.send("{\"type\":\"stop_stream\"}");
+    }
+
+    /**
+     * Called ONLY by the ObsWebsocketGateway when it receives a "Streaming State Message" from an OBS Embed.
+     *
+     * @param streaming true if streaming, false if not streaming
+     */
+    public void setStreaming(boolean streaming) {
+        log("Streaming State Changed | " + isStreaming + " -> " + streaming);
+        isStreaming = streaming;
+        dispatchStreamingStateChange();
+    }
+
+    /**
+     * Called ONLY by the ObsWebsocketGateway when it receives a "Scene Changed Event" from an OBS Embed.
+     *
+     * @param sceneName the name of the scene that the user has changed to
+     */
+    public void setScene(String sceneName) {
+        log("Scene State Changed | " + currentScene + " -> " + sceneName);
+        currentScene = sceneName;
+        dispatchSceneChange();
+    }
+
+    /**
+     * Closes the socket connection to the OBS Embed.
+     * Called ONLY by ObsEmbedSessions for good housekeeping.
+     */
+    void closeSocket() {
+        log("Closing Socket...");
+        webSocket.close();
+    }
+
+    private void log(String message) {
+        System.out.println("OBS Embed Controller | " + hostAddress + " | " + message);
+    }
+
+    /**
+     * Dispatches the "Streaming State Change" event to any listeners.
+     */
+    private void dispatchStreamingStateChange() {
+        if(stateChangeListener != null) {
+            log("Dispatching Streaming State Change Event...");
+            stateChangeListener.accept(isStreaming);
+        }
+    }
+
+    /**
+     * Dispatches the "Scene Change" event to any listeners.
+     */
+    private void dispatchSceneChange() {
+        if(sceneChangeListener != null) {
+            log("Dispatching Scene Change Event...");
+            sceneChangeListener.accept(currentScene);
+        }
+    }
+
+}
---
+++ ObsWebsocketGateway/src/main/java/algo/obs/websocket/SecureWebSocketServer.java
@@ -0,0 +1,60 @@
+package algo.obs.websocket;
+
+import org.java_websocket.server.DefaultSSLWebSocketServerFactory;
+import org.java_websocket.server.WebSocketServer;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.security.KeyStore;
+
+public abstract class SecureWebSocketServer extends WebSocketServer {
+
+    private static final String
+            KEY_STORE_PATH = "keystore.jks", // Adjust this path
+            KEY_STORE_PASSWORD = System.getenv("KEY_STORE_PASSWORD");
+
+
+    public SecureWebSocketServer(int port) {
+        super(new InetSocketAddress("0.0.0.0", port));
+        setupSecureContext();
+    }
+
+    private void setupSecureContext() {
+
+        try {
+
+            // sanity check: validate the keystore exists on the local machine
+            if(!new File(KEY_STORE_PATH).exists()) {
+                System.out.println("Warning. Key Store Does Not Exist: " + KEY_STORE_PATH + ". The Websocket Server can still function, but it is NOT SECURE.");
+                return;
+            }
+
+            // Load the JDK's key store
+            final KeyStore ks = KeyStore.getInstance("JKS");
+            try (InputStream in = new FileInputStream(KEY_STORE_PATH)) {
+                ks.load(in, KEY_STORE_PASSWORD.toCharArray());
+            }
+
+            // Set up key manager factory to use our key store
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(ks, KEY_STORE_PASSWORD.toCharArray());
+
+            // Initialize the SSLContext to work with our key managers.
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(kmf.getKeyManagers(), null, null);
+
+            // Formally switch the WebSocket to secure mode.
+            setWebSocketFactory(new DefaultSSLWebSocketServerFactory(sslContext));
+
+        } catch (Exception e) {
+            System.out.println("Failed to setup secure context");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
